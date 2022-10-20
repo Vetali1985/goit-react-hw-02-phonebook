116a78665961c80a76764f99e5b32ba5db7d3322
Vetali1985 <100028292+Vetali1985@users.noreply.github.com>
2022-10-20 21:46:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,11 +7,23 @@ class App extends Component  {
     contacts: [],
     name: ''
   };
-
+  handleInputChange = e => {
+    this.setState({
+    [e.currentTarget.name]:[e.currentTarget.value]
+  })
+}
   render() {
     
     return (
-      null
+<input
+  type="text"
+        name="name"
+        value={this.state.name}
+        onChange={this.handleInputChange}
+  pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+  title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+  required
+/>
       
     );
     
